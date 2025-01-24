31efd858d8dbeed6954abc782b44c10694996b10
Daniil <daniilshahov33@gmail.com>
2025-01-24 10:10:23 +0200

    Solve fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function (begin, end) {
+  if (begin < 0 || end < 0) return 'ERROR';
+  if (Math.floor(begin) !== begin || Math.floor(end) !== end) return 'ERROR';
+  if (begin === NaN || end === NaN) return 'ERROR';
+  if (begin > end) [begin, end] = [end, begin]; // swaps variables without using a temporary one
 
+  let result = 0;
+  for (let i = begin; i <= end; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
