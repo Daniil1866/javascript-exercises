a4f8ee31c50db41925ad7a075fda14677197748a
Daniil <daniilshahov33@gmail.com>
2025-01-24 09:50:47 +0200

    Solve third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (orig) {
+  let reverse = '';
+  for (let i = orig.length - 1; i >= 0; i--) {
+    reverse += orig[i];
+  }
+  return reverse;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
